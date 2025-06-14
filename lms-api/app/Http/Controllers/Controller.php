d0e851879a4bd9f89f516b326f4f512f210b336d
Juan Andres Caballero <jacaballero18@gmail.com>
2025-06-14 10:14:58 -0500

    feat: Instalación de paquetes iniciales

--- lms-api/app/Http/Controllers/Controller.php
+++ lms-api/app/Http/Controllers/Controller.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="Título de tu API",
+ *     version="1.0.0",
+ *     description="Descripción de tu API"
+ * )
+ */
 abstract class Controller
 {
     //
